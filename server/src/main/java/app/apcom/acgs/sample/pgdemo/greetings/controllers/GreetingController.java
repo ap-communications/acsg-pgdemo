da881ef402b4598be321cd0629cc686889a9af97
Hitoshi Kamezaki <kamezaki@ap-com.co.jp>
2021-04-01 19:50:18 +0900

    output every severity log

--- server/src/main/java/app/apcom/acgs/sample/pgdemo/greetings/controllers/GreetingController.java
+++ server/src/main/java/app/apcom/acgs/sample/pgdemo/greetings/controllers/GreetingController.java
@@ -5,13 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 import app.apcom.acgs.sample.pgdemo.greetings.models.GreetingResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/greetings")
 public class GreetingController {
     @RequestMapping()
     public GreetingResponse get() {
+        log.error("error logging", new Exception("this is sample expeption"));
+        log.warn("warn logging");
+        log.info("info logging");
+        log.debug("debug logging");
         return GreetingResponse.builder().message("Haho").build();
     }
 }
