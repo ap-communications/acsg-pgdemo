fa70c1b6461b3609dd8a5aecd7026bb85a68a25a
Hitoshi Kamezaki <kamezaki@ap-com.co.jp>
2021-04-15 20:26:46 +0900

    add task list controller

--- server/src/main/java/app/apcom/acgs/sample/pgdemo/greetings/controllers/TaskController.java
+++ server/src/main/java/app/apcom/acgs/sample/pgdemo/greetings/controllers/TaskController.java
@@ -1,9 +1,14 @@
 package app.apcom.acgs.sample.pgdemo.greetings.controllers;
 
+import java.time.ZoneId;
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import app.apcom.acgs.sample.pgdemo.greetings.models.GreetingResponse;
+import app.apcom.acgs.sample.pgdemo.greetings.dao.TaskDao;
+import app.apcom.acgs.sample.pgdemo.greetings.models.TaskListItem;
+import app.apcom.acgs.sample.pgdemo.greetings.models.TaskListResponse;
 import app.apcom.acgs.sample.pgdemo.greetings.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,9 +21,23 @@ public class TaskController {
     private final TaskRepository taskRepository;
     
     @RequestMapping()
-    public GreetingResponse get() {
+    public TaskListResponse get() {
         log.debug("debug logging");
-        var itr = taskRepository.findAll();
-        return GreetingResponse.builder().message("Haho").build();
+        var itr = taskRepository.findAll().iterator();
+        var list = new ArrayList<TaskListItem>();
+        while(itr.hasNext()) {
+            list.add(convertFrom(itr.next()));
+        }
+        return TaskListResponse.builder().items(list).build();
+    }
+
+    private TaskListItem convertFrom(TaskDao task) {
+        return TaskListItem.builder()
+            .name(task.getName())
+            .createdAt(
+                task.getCreatedAt()
+                    .atZone(ZoneId.systemDefault()).toEpochSecond())
+            .updatedAt(task.getUpdatedAt().atZone(ZoneId.systemDefault()).toEpochSecond())
+            .build();
     }
 }
